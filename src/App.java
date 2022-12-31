82b968875ad274600e373211c44a1d59cc711012
Matthew Hickson <matthewhickson@outlook.com>
2022-12-31 00:37:20 -0500

    Woah! XML parsing of OPML files... ugly, but a start

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 import javax.swing.UIManager;
-// import java.nio.charset.Charset;
-// import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 
-// import Controllers.OpmlConverter;
+import Controllers.OpmlConverter;
 import Views.MainForm;
 
 public class App {
@@ -19,6 +19,8 @@ public class App {
 
         // String opml = OpmlConverter.ReadOpmlFileAsString(Paths.get("{pathtoopml}"), Charset.forName("UTF-8"));
         // System.out.println(opml);
+
+        // Object x = OpmlConverter.BuildSubscriptionListFromOpmlString(opml);
 
         // OpmlConverter.WriteOpmlStringToFile(opml, Paths.get("{pathtoopml}"), Charset.forName("UTF-8"), false);
     }
--- src/Controllers/OpmlConverter.java
+++ src/Controllers/OpmlConverter.java
@@ -3,9 +3,26 @@ package Controllers;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class OpmlConverter {
     
@@ -35,6 +52,84 @@ public class OpmlConverter {
         }
 
         return success;
+    }
+
+    // YUCK!
+    // FIX: make this less like garbage...
+    public static Object BuildSubscriptionListFromOpmlString(String s) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        XPathFactory factoryx = XPathFactory.newInstance();
+
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+
+            try {
+                InputSource is = new InputSource(new StringReader(s));
+                Document doc = builder.parse(is);
+
+                XPath xpath = factoryx.newXPath();
+                try {
+                    XPathExpression expr = xpath.compile("/opml/body/outline[@text='feeds']/outline");
+
+                    NodeList list = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+                    for (int i = 0; i <= list.getLength(); i++) {
+                        Node n = list.item(i);
+                        
+                        if (n != null) {
+                            if (n.hasAttributes()) {
+                                NamedNodeMap m = n.getAttributes();
+    
+                                Node an1 = m.getNamedItem("text");
+                                Node an2 = m.getNamedItem("xmlUrl");
+                                Node an3 = m.getNamedItem("type");
+    
+                                boolean printedSomething = false;
+    
+                                if (an1 != null) {
+                                    System.out.print(an1.getTextContent());
+                                    printedSomething = true;
+                                }
+    
+                                if (an2 != null) {
+                                    if (printedSomething) {
+                                        System.out.print(" : ");
+                                    }
+                                    System.out.print(an2.getTextContent());
+                                    printedSomething = true;
+                                }
+    
+                                if (an3 != null) {
+                                    if (printedSomething) {
+                                        System.out.print(" : ");
+                                    }
+                                    System.out.print(an3.getTextContent());
+                                    printedSomething = true;
+                                }
+    
+                                if (printedSomething) {
+                                    System.out.println();
+                                }
+                            }
+                        }
+                    }
+
+                    // TODO: create actual subscriptions from NodeList...
+                    return list;
+                } catch (XPathExpressionException e) {
+                    e.printStackTrace();
+                }
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
     }
 
 }
